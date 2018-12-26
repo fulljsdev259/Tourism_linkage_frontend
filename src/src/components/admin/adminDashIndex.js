040eb216c9d4a4369e3b00e3d9641afb7c050348
gmchaturvedi <gmchaturvedi@gmail.com>
2018-12-26 18:45:53 +0530

    delete and vupdate on admin record wise basic done

--- src/src/components/admin/adminDashIndex.js
+++ src/src/components/admin/adminDashIndex.js
@@ -2,15 +2,16 @@ import React from 'react';
 import SpicyDatatable from 'spicy-datatable';
 import { withRouter } from 'react-router-dom'
 
+import { Modal, Button } from 'antd';
 import './datatable.scss';
 
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const query = gql`{
+export const query = gql`{
    
         party{
-         
+            _id
              name
              region
              categories
@@ -20,89 +21,149 @@ const query = gql`{
   }
   `
 
+export const mutation = gql` mutation deleteVendor($dataId:String){
+   
+        deleteVendor(dataId:$dataId){
+               party{
+                   name
+               }
+               errors
+        }
+        
+   
+  }
+  `
 
-const AdminDash = ( { data, match, history } ) => {
 
-    if ( data.loading ) {
-        return <span>Loading ..</span>
+
+
+class AdminDash extends React.Component {
+    state = { visible: false, dataId: '' }
+
+    showModal = () => {
+
+        this.setState( {
+            visible: true,
+        } );
     }
-    console.log( data )
-    let row1 = [];
-    data.party.map( ( data, i ) => {
-        //const c=data.passPercentage;
-        //console.log( data )
 
 
 
-        row1.push( ...[{
-            id: i + 1, name: data.name, region: data.region, categories: data.categories,
-            action: <div style={ { display: 'flex', justifyContent: 'center' } }>
-                <button onClick={ () => history.push( `${ match.url }/editSupplier/${ data.name }` ) }
-                    style={ {
-                        fontSize: 17, border: 'none', borderRadius: 5,
-                        backgroundColor: '#1289A7', color: '#fff', padding: "7px 15px"
-                    } }>Edit</button>
-                <button
-                    onClick={ () => history.push( `${ match.url }/english/${ data.name }` ) }
-                    style={ { fontSize: 17, border: 'none', borderRadius: 5, marginLeft: 10, color: '#fff', backgroundColor: '#006266', padding: "7px 15px" } }>Delete</button>
-            </div>
-        }] )
-        return row1;
-    } )
+    handleOk = async ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        const deleteVendor = await this.props.mutate( {
+            variables: { dataId: this.state.dataId },
+            refetchQueries: [{ query: query }]
+        } )
 
-    const columns = [
-        {
-            key: 'id',
-            label: 'Sr No',
-        }, {
-            key: 'name',
-            label: 'Name',
-        },
-        {
-            key: "region",
-            label: "Region"
-        },
-        {
-            key: "categories",
-            label: "Category"
-        },
+        //   this.props.history.push( '/admin' )
+    }
 
-        {
-            key: 'action',
-            label: 'Degree',
-        },
+    handleCancel = ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        // this.props.history.push( '/admin' )
+    }
+
+    render() {
+        const { data, match, history } = this.props;
+        if ( data.loading ) {
+            return <span>Loading ..</span>
+        }
+        console.log( data )
+        let row1 = [];
+        data.party.map( ( data, i ) => {
+            //const c=data.passPercentage;
+            //console.log( data )
 
 
-    ];
 
-    const customOptions = {
-        itemsPerPageOptions: [6, 15, 20, 25, 30],
-        itemsPerPageLabel: 'Page :',
-        nextPageLabel: '>',
-        previousPageLabel: '<',
-        searchLabel: ' ',
-        searchPlaceholder: 'Search here…',
-        noEntriesLabel: 'No Entries Found',
-        entryCountLabels: ['Showing: ', 'to', 'of', ''],
-        showDownloadCSVButton: true,
-        downloadCSVButtonLabel: 'Download CSV',
+            row1.push( ...[{
+                id: i + 1, name: data.name, region: data.region, categories: data.categories,
+                action: <div style={ { display: 'flex', justifyContent: 'center' } }>
+                    <button onClick={ () => history.push( `${ match.url }/editSupplier/${ data._id }` ) }
+                        style={ {
+                            fontSize: 17, border: 'none', borderRadius: 5,
+                            backgroundColor: '#1289A7', color: '#fff', padding: "7px 15px"
+                        } }>Edit</button>
+                    <button
+                        onClick={ () => {
+                            // alert( 'hello' )
+                            this.setState( { dataId: data._id, visible: true } )
+                            //this.showModal();
+                        } }
+                        style={ { fontSize: 17, border: 'none', borderRadius: 5, marginLeft: 10, color: '#fff', backgroundColor: '#006266', padding: "7px 15px" } }>Delete</button>
+                </div>
+            }] )
+            return row1;
+        } )
 
-    };
+        const columns = [
+            {
+                key: 'id',
+                label: 'Sr No',
+            }, {
+                key: 'name',
+                label: 'Name',
+            },
+            {
+                key: "region",
+                label: "Region"
+            },
+            {
+                key: "categories",
+                label: "Category"
+            },
+
+            {
+                key: 'action',
+                label: 'Degree',
+            },
 
 
-    return <div style={ { maxHeight: '100%', overflowY: "scroll" } }>
-        <h2>Suppliers</h2>
-        <SpicyDatatable
-            tableKey="demo-table-genral"
-            // see below for prop documentation
-            columns={ columns }
-            rows={ row1 }
-            // optional, used to override chosen default settings/labels
-            config={ customOptions }
-        />
+        ];
 
-    </div>
+        const customOptions = {
+            itemsPerPageOptions: [6, 15, 20, 25, 30],
+            itemsPerPageLabel: 'Page :',
+            nextPageLabel: '>',
+            previousPageLabel: '<',
+            searchLabel: ' ',
+            searchPlaceholder: 'Search here…',
+            noEntriesLabel: 'No Entries Found',
+            entryCountLabels: ['Showing: ', 'to', 'of', ''],
+            showDownloadCSVButton: true,
+            downloadCSVButtonLabel: 'Download CSV',
+
+        };
+
+
+        return <div style={ { maxHeight: '100%', overflowY: "scroll" } }>
+            <h2>Suppliers</h2>
+            <SpicyDatatable
+                tableKey="demo-table-genral"
+                // see below for prop documentation
+                columns={ columns }
+                rows={ row1 }
+                // optional, used to override chosen default settings/labels
+                config={ customOptions }
+            />
+  <Modal
+                title="Notification"
+                visible={ this.state.visible }
+                onOk={ this.handleOk }
+                onCancel={ this.handleCancel }
+            >
+Are You Sure to delete this record ?
+                
+            </Modal>
+        </div>
+    }
 }
 
-
-export default withRouter( graphql( query )( AdminDash ) );
+export default withRouter( graphql( query )( graphql( mutation )( AdminDash ) ) );
--- src/src/components/admin/editParty.js
+++ src/src/components/admin/editParty.js
@@ -6,6 +6,10 @@ import { Field, reset, reduxForm } from 'redux-form';
 import { renderField, renderFieldTextArea } from './inputComponent'
 import './editrecord.scss'
 
+import { Modal, Button } from 'antd';
+
+import { query as query1} from './adminDashIndex';
+
 const form = reduxForm( {
     form: 'editSupplier',
     enableReinitialize: true,
@@ -16,16 +20,49 @@ const form = reduxForm( {
 const query = gql` query singleParty($name:String){
    
         singleParty(name:$name){
-         
+         _id
              name
              region
              categories
              address
+             phoneNumber
+             website
+             tags
         }
         
    
   }
   `
+/*$email:String,$tags: String,
+, $categories: String, $region: String, $description: String, $email: String,
+$address: String,
+$phoneNumber: String, $website: String
+
+categories:$categories,description:$description,phoneNumber:$phoneNumber,tags:$tags,
+    region:$region,website:$website,address:$address,
+
+*/
+
+const mutation = gql`mutation editVendor1($dataId:String,$name:String,$tags: String,
+, $categories: String, $region: String, $description: String, 
+$address: String,
+$phoneNumber: String, $website: String
+){
+        editVendor(name:$name,dataId:$dataId,
+        categories:$categories,description:$description,phoneNumber:$phoneNumber,tags:$tags,
+    region:$region,website:$website,address:$address
+        
+        ){
+            party{
+                name
+            }
+            errors
+                
+            
+        }
+    }
+
+`
 
 
 
@@ -37,62 +74,146 @@ const query = gql` query singleParty($name:String){
 
 
 
+class EditRecord extends React.Component {
+    state = { visible: false }
 
-const EditRecord = ( { match } ) => {
-    return <div className="editRecord">
+    showModal = () => {
+        this.setState( {
+            visible: true,
+        } );
+    }
 
-        <h2>Edit Record</h2>
 
-        <div className="row">
-            <div className="col1">
-                <div className="label">Name</div>
-                <div className="input">
-                    <Field name='name' component={ renderField } type="text" label="Name" />
 
+    handleOk = ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        this.props.history.push('/admin')
+    }
+
+    handleCancel = ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        this.props.history.push( '/admin' )
+    }
+    render() {
+        const { match, mutate, handleSubmit, id } = this.props;
+        return <div className="editRecord">
+
+            <h2>Edit Record</h2>
+
+            <div className="row">
+                <div className="col1">
+                    <div className="label">Name</div>
+                    <div className="input">
+                        <Field name='name' component={ renderField } type="text" label="Name" />
+
+                    </div>
+                </div>
+                <div className="col2">
+                    <div className="label">Region</div>
+                    <div className="input">
+                        <Field name='region' component={ renderField } type="text" label="Region" />
+
+                    </div>
                 </div>
             </div>
-            <div className="col2">
-                <div className="label">Region</div>
-                <div className="input">
-                    <Field name='region' component={ renderField } type="text" label="Region" />
+            <div className="row">
+                <div className="col1">
+                    <div className="label">Category</div>
+                    <div className="input">
+                        <Field name='categories' component={ renderField } type="text" label="Category" />
 
+                    </div>
+                </div>
+                <div className="col2">
+                    <div className="label">Address</div>
+                    <div className="input">
+                        <Field name='address' component={ renderFieldTextArea } type="text" label="Address" />
+
+                    </div>
                 </div>
             </div>
+            <div className="row">
+                <div className="col1">
+                    <div className="label">Phone Number</div>
+                    <div className="input">
+                        <Field name='phoneNumber' component={ renderField } type="text" label="Phone Number" />
+
+                    </div>
+                </div>
+                <div className="col2">
+                    <div className="label">Website</div>
+                    <div className="input">
+                        <Field name='website' component={ renderField } type="text" label="Website" />
+
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col1">
+                    <div className="label">Description</div>
+                    <div className="input">
+                        <Field name='description' component={ renderFieldTextArea } type="text" label="Description" />
+
+                    </div>
+                </div>
+                <div className="col2">
+                    <div className="label">Tags(seprate by comma)</div>
+                    <div className="input">
+                        <Field name='tags' component={ renderFieldTextArea } type="text" label="tags" />
+
+                    </div>
+                </div>
+            </div>
+
+            <Modal
+                title="Notification"
+                visible={ this.state.visible }
+                onOk={ this.handleOk }
+                onCancel={ this.handleCancel }
+            >
+            Data has been edited successfully
+                
+            </Modal>
+            <button className="buttonSave" onClick={ handleSubmit( async ( data ) => {
+                console.log( mutate )
+                const editData = await mutate( {
+                    variables: {
+                        name: data.name, dataId: id, tags: data.tags, address: data.address, phoneNumber: data.phoneNumber,
+                        region: data.region, categories: data.categories, website: data.website, description: data.description
+                    },
+                    refetchQueries: [{query:query1}]
+                } )
+
+                this.showModal();
+
+
+            } ) }>Save</button>
+            <button className="buttonBack">Back</button>
+
+
+
         </div>
-        <div className="row">
-            <div className="col1">
-                <div className="label">Category</div>
-                <div className="input">
-                    <Field name='categories' component={ renderField } type="text" label="Category" />
-
-                </div>
-            </div>
-            <div className="col2">
-                <div className="label">Address</div>
-                <div className="input">
-                    <Field name='address' component={ renderFieldTextArea } type="text" label="Address" />
-
-                </div>
-            </div>
-        </div>
-
-
-
-    </div>
+    }
 }
+const EditRecordForm = form( graphql( mutation )( EditRecord ) )
 
-const EditRecordForm = form( EditRecord )
-
-const EditRecordWrapper = ( { match, data } ) => {
+const EditRecordWrapper = ( {  data,history } ) => {
     if ( data.loading ) {
         return <span>Loading ....</span>
     }
     const initialValues = {
         name: data.singleParty.name, categories: data.singleParty.categories, address: data.singleParty.address,
-        region: data.singleParty.region
+        region: data.singleParty.region, phoneNumber: data.singleParty.phoneNumber, website: data.singleParty.website,
+        description: data.singleParty.description, tags: data.singleParty.tags
     }
     console.log( data )
-    return <EditRecordForm initialValues={ initialValues } />
+    return <EditRecordForm initialValues={ initialValues } id={ data.singleParty._id } history={ history} />
 
 
 }
@@ -106,4 +227,4 @@ export default withRouter( graphql( query, {
         return { variables: { name: match.params.name } }
     }
 
-} )( form( EditRecordWrapper ) ) );
+} )( EditRecordWrapper ) );
