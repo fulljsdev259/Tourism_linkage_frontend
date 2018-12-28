39eca0ff742379d902736beca67aaaddf25ef8b3
gmchaturvedi <gmchaturvedi@gmail.com>
2018-12-28 17:09:53 +0530

    signup feature new user create

--- src/src/components/register/signUp/index.js
+++ src/src/components/register/signUp/index.js
@@ -1,17 +1,134 @@
 import React from 'react';
 import './index.scss'
+import { withRouter } from 'react-router-dom';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Field, reset, reduxForm } from 'redux-form';
+import { receiveLogin } from '../../../action/auth';
+import { validate } from '../validate';
 
-export default () => {
-    return <div className="signup">
-        <div className="arrow-up"></div>
+import { Modal as Modal1, Button } from 'antd';
 
-        <input placeholder="Full Name" />
-        <input placeholder="Email" />
-        <input placeholder="Password" />
 
-        <input placeholder="Retype password" />
-        <button className="button" >
-            Register
-        </button>
+const form = reduxForm( {
+    form: 'signup',
+    enableReinitialize: true,
+    validate
+
+} )
+
+
+
+export const renderField = ( { input, label, type, meta: { touched, error } } ) => (
+    <div style={ { display: 'flex', flexDirection: 'column' } }>
+        <input { ...input } placeholder={ label } type={ type } />
+
+        <span style={ { color: '#fff', texAlign: 'center' } }>{ touched && error && <span>{ error }</span> }</span>
     </div>
+)
+
+
+
+const mutation = gql`mutation signup($email:String,$password:String,$name:String){
+    signUp(email:$email,password:$password,name:$name){
+        user{
+            email
+            role
+        }
+        errors
+
+    }
+}`
+
+
+
+class Signup extends React.Component {
+    state = { visible: false, errors: [] }
+
+    showModal = () => {
+        this.setState( {
+            visible: true,
+        } );
+    }
+
+
+
+    handleOk = ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        //      this.props.history.push( '/admin' )
+    }
+
+    handleCancel = ( e ) => {
+        console.log( e );
+        this.setState( {
+            visible: false,
+        } );
+        //        this.props.history.push( '/admin' )
+    }
+
+    render() {
+        const { mutate, history, handleSubmit } = this.props;
+
+        return <div className="signup">
+            <Modal1
+                title="Notification"
+                visible={ this.state.visible }
+                onOk={ this.handleOk }
+                onCancel={ this.handleCancel }
+            >
+                { this.state.errors.map( ( d, i ) => <div key={ i }>{ d }</div> ) }
+
+            </Modal1>
+
+            <div className="arrow-up"></div>
+
+            <Field name='name' component={ renderField } type="text" label="Full Name" />
+            <Field name='email' component={ renderField } type="text" label="Email" />
+
+            <Field name='password' component={ renderField } type="password" label="Password" />
+
+
+            <Field name='retype' component={ renderField } type="password" label="Retype password" />
+            <button className="button"
+
+                onClick={ handleSubmit( async ( data ) => {
+
+                    try {
+                        const result = await mutate( {
+                            variables: {
+                                email: data.email, password: data.password, name: data.name
+                            }
+                        } )
+
+                        if ( result.data.signUp.errors.length > 0 ) {
+
+                            this.setState( { errors: result.data.signUp.errors, } )
+                            this.showModal()
+                            //commonError()
+                        }
+                        else {
+
+
+                            this.setState( { errors: ["You have successfully regitered"] } )
+                            this.showModal()
+                        }
+                    }
+                    catch ( err ) {
+                        console.log( err );
+                    }
+
+
+
+                } ) }
+            >
+                Register
+        </button>
+        </div>
+    }
 }
+
+
+export default withRouter( form( graphql( mutation )( Signup ) ) )
---
+++ src/src/components/register/validate.js
@@ -0,0 +1,37 @@
+import Moment from "moment";
+
+export const validate = values => {
+    const errors = {}
+    if ( !values.saluation ) {
+        errors.saluation = "Salutation is Required"
+    }
+
+    if ( !values.password ) {
+        errors.password = "Password is Required"
+    }
+
+    if ( !values.retype ) {
+        errors.retype = "Retype Password is Required"
+    }
+
+    if ( !values.email ) {
+        errors.email = 'Required'
+    } else if ( !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test( values.email ) ) {
+        errors.email = 'Invalid email address'
+    }
+
+
+    if ( !values.name ) {
+        errors.name = "Full Name is  Required"
+    }
+
+    if ( values.password && values.retype ) {
+        if ( values.password !== values.retype ) {
+            errors.retype = "Password and Retype password should be same"
+        }
+
+    }
+    return errors
+}
+
+//export default validate
