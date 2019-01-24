16f3d491013b48bf0dad575328c0cb20ac1866f6
rahul_excell <rahul.excel2011@gmail.com>
2019-01-24 15:10:09 +0530

    changes done for https

--- src/src/config/constant.js
+++ src/src/config/constant.js
@@ -1,3 +1,4 @@
-// export const url = 'http://localhost:9411'
+// export const url = 'http://localhost:9411/graphql'
 // export const url = 'http://alexdemo.go4itest.com/api'
-export const url = 'http://ec2-3-17-143-130.us-east-2.compute.amazonaws.com:9411'
+// export const url = 'http://ec2-3-17-143-130.us-east-2.compute.amazonaws.com:9411'
+export const url = 'https://jamaicatourismsuppliersdirectory.com/graphql'
